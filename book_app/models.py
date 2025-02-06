7a67a6fb42d42b5614bf290ea2d791c7e70e85ef
Alexander Terekhov <cahdro2155@gmail.com>
2025-02-06 13:42:33 +0300

    Изменение и удаление записей в db

--- book_app/models.py
+++ book_app/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f'{self.title} - {self.rating}'
+
+
+# from book_app.models import Book
